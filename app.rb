9248208cd8b2afad882d090a87d80da1dee84ce1
kha le & cody bouscaren <student@epicodus.com>
2015-05-19 14:27:14 -0700

    Will add logic to avoid same cats fighting

--- app.rb
+++ app.rb
@@ -14,13 +14,17 @@ post('/') do
 end
 
 get('/game') do
+  @cats = Cat.all().order(wins: :desc)
   @kitty1 = Cat.find(rand(1..3))
   @kitty2 = Cat.find(rand(1..3))
-
+  if @kitty1 == @kitty2
+    redirect('/game')
+  end
   erb(:game)
 end
 
 patch('/game') do
+  @cats = Cat.all().order(wins: :desc)
   @winning_kitty = Cat.find(params.fetch("winning_id").to_i)
   @losing_kitty = Cat.find(params.fetch("losing_id").to_i)
   win = @winning_kitty.wins
