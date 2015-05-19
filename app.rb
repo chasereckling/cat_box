79ed7739158f4c56d64f9d1ee3f1d5993ca80313
kha le & cody bouscaren <student@epicodus.com>
2015-05-19 13:46:46 -0700

    third kitty

--- app.rb
+++ app.rb
@@ -7,44 +7,26 @@ get('/') do
 end
 
 post('/') do
-  Cat.create({:name => "tbone", :wins => 0, :loss => 0, :image => "img/tbone.png"})
-  Cat.create({:name => "Freckles", :wins => 0, :loss => 0, :image => "img/freckles.png"})
+  Cat.create({:name => "Tbone", :wins => 0, :loss => 0, :image => "http://s18.postimg.org/x9s3ehc09/tbone.jpg"})
+  Cat.create({:name => "Freckles", :wins => 0, :loss => 0, :image => "http://s8.postimg.org/rhzsiqluc/freckles.jpg"})
+  Cat.create({:name => "Truman", :wins => 0, :loss => 0, :image => "http://s11.postimg.org/k9osn032b/truman.jpg"})
   redirect('/game')
 end
 
 get('/game') do
-  @kitty1 = Cat.find(rand(1..2))
-  @kitty2 = Cat.find(rand(1..2))
+  @kitty1 = Cat.find(rand(1..3))
+  @kitty2 = Cat.find(rand(1..3))
 
 erb(:game)
 end
 
 patch('/game') do
   @winning_kitty = Cat.find(params.fetch("winning_id").to_i)
-  @losing_kitty = Cat.find(params.fetch("losing_id"))
+  @losing_kitty = Cat.find(params.fetch("losing_id").to_i)
   win = @winning_kitty.wins
   loss = @losing_kitty.loss
 
   @winning_kitty.update(:wins => win + 1)
   @losing_kitty.update(:loss => loss + 1)
-
-  # @kitty1 = Cat.find(1)
-  # @kitty2 = Cat.find(2)
-  # @choice = params.fetch("choice")
-  #
-  # if @choice == "left_cat"
-  #
-  #   win = @kitty1.wins
-  #   loss = @kitty2.loss
-  #   @kitty1.update(:wins => win + 1 )
-  #   @kitty2.update(:loss => loss + 1 )
-  #
-  # else
-  #
-  #   win = @kitty2.wins.to_i
-  #   loss = @kitty1.loss.to_i
-  #   @kitty2.update(:wins => win + 1 )
-  #   @kitty1.update(:loss => loss + 1 )
-  # end
   redirect('/game')
 end
