10c484e9b04ec9cef0e36590c5be1fcb49d5a79b
chase & Jessica Engel <student@epicodus.com>
2015-05-20 11:24:24 -0700

    Epic changes:Added upload page. Game changer

--- app.rb
+++ app.rb
@@ -6,6 +6,26 @@ get('/') do
   erb(:index)
 end
 
+get('/upload') do
+  erb(:upload)
+end
+
+post('/upload') do
+  name = params.fetch("name")
+  bio = params.fetch("bio")
+  photo = params.fetch("photo")
+  new_cat = Cat.create({:name => name, :wins => 0, :loss => 0, :image => photo, :bio => bio})
+  @cats = Cat.all().order(wins: :desc)
+  @kitty1 = Cat.find(rand(1..@cats.length))
+  @kitty2 = Cat.find(rand(1..@cats.length))
+  if @kitty1 == @kitty2
+    redirect('/game')
+  end
+  erb(:game)
+end
+
+
+
 get('/game') do
   @cats = Cat.all().order(wins: :desc)
   @kitty1 = Cat.find(rand(1..@cats.length))
