76aa2af3f42abb0644e59b70a267fd706e744b4d
chase <chase.reckling@gmail.com>
2015-05-21 14:33:25 -0700

    deleted friend class, addede cats to cat

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,5 +1,8 @@
 class Cat < ActiveRecord::Base
   validates_presence_of(:name)
   has_many(:comments)
-  has_and_belongs_to_many(:friends)
+  has_and_belongs_to_many(:cats,
+  :join_table => "cat_friends",
+  :foreign_key => "cat_id_1",
+  :association_foreign_key => "cat_id_2")
 end
